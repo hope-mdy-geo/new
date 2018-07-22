8cbe7f14f034feae1ce90e2d5e12cd8c8277d557
EiEiKyaw <eieikyaw20101968@gmail.com>
2018-07-22 03:26:38 -0800

    Converted (Win to Uni) and (Uni to Win)

--- W2U_U2W/main.py
+++ W2U_U2W/main.py
@@ -14,7 +14,7 @@ input_file = codecs.open(input_file_name,encoding='utf-8')
 output_file = codecs.open(output_file_name,encoding='utf-8', mode='w')
 
 for input_line in input_file:
-  input_line = win2uni.convert(input_line)
+  #input_line = win2uni.convert(input_line)
   input_line = uni2win.convert(input_line)
   output_file.write(input_line)
   output_file.flush()
