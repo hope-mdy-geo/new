0c2a54d75d90d81cd2ac2279f25d6789413b07a9
EiEiKyaw <eieikyaw20101968@gmail.com>
2018-08-22 21:43:25 -0800

    Support to find convert error

--- testunitowin.py
+++ testunitowin.py
@@ -3,8 +3,10 @@ import uni2win
 
 class TestZG2UNI(unittest.TestCase):
     def test_article_one(self):
-        win = u'''pdwfwdkh&Sdjuí'''
-        unicode = u'''စိတ်တို့ရှိကြ၍'''
+        win = u'''vlwdkif;onf wlnD vGwfvyfaom *kPfodu©mjzihf vnf;aumif;? wlnDvGwfvyfaom tcGihfta&;rsm;jzihf vnf;aumif;? arG;zGm;vmolrsm; jzpfonf/ 
+        xdkolwdkYü ydkif;jcm; a0zefwwfaom ÓPfESihf usihf0wf odwwfaom pdwfwdkY½SdMuí xdkolwdkYonf tcsif;csif; arwÅmxm;í qufqHusihfokH;oihf\/'''
+        unicode = u'''လူတိုင်းသည် တူညီ လွတ်လပ်သော ဂုဏ်သိက္ခာဖြင့် လည်းကောင်း၊ တူညီလွတ်လပ်သော အခွင့်အရေးများဖြင့် လည်းကောင်း၊ မွေးဖွားလာသူများ ဖြစ်သည်။ 
+        ထိုသူတို့၌ ပိုင်းခြား ဝေဖန်တတ်သော ဉာဏ်နှင့် ကျင့်ဝတ် သိတတ်သော စိတ်တို့ရှိကြ၍ ထိုသူတို့သည် အချင်းချင်း မေတ္တာထား၍ ဆက်ဆံကျင့်သုံးသင့်၏။'''
         result = uni2win.convert(unicode)
         self.assertEqual(win, result, "Failed converting Article One")
 
